622f7a91b11ec33db014340be447065236d15661
yutax77 <yutax77@gmail.com>
2011-10-29 09:19:30 +0900

    initial commit

---
+++ src/test/scala/com/yutax77/lrucache/LruCacheTest.scala
@@ -0,0 +1,11 @@
+package com.yutax77.lrucache
+import org.scalatest.junit.JUnitSuite
+import org.scalatest.junit.ShouldMatchersForJUnit
+import org.junit.Test
+
+class LruCacheTest extends JUnitSuite with ShouldMatchersForJUnit {
+	@Test
+	def dummy() {
+	  true should be (true)
+	}
+}
